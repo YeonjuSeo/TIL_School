c49f670e7feb4c156f57fb694da255ee46aa6f8b
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-04-02 16:54:12 +0900

    Create 동물테스트.java for OOP

---
+++ JAVA/Basics/동물테스트.java
@@ -0,0 +1,11 @@
+
+public class 동물테스트 {
+	public static void main(String [] args) {
+		int[][] ar=new int[5][5];
+		
+		동물 animal = new 동물("야옹이",3);
+		animal.놀기();
+		animal.놀기(10);
+		animal.먹기();
+	}
+}
